fa7e8dba62bce13a0667e44cccfb18abefe71e04
Tom Sixma <sixmatom@gmail.com>
2019-10-17 14:45:45 +0200

    behave update

--- features/steps/loadgame.py
+++ features/steps/loadgame.py
@@ -1,3 +1,7 @@
 from behave import given, when, then  # pylint: disable=no-name-in-module
 
 from game import Game
+
+@given(u'the user starts the game')
+@when(u'the user types the correct path python shell.py {game.json:d}')
+@then(u'the games should load the correct json file')
